88c9962cb8b39342db2f132846dd30eeb90e1f5b
Peter Heylin <peter@peterheylin.com>
2018-05-09 10:02:26 +0100

    20180509 updated layout

--- index.php
+++ index.php
@@ -18,10 +18,11 @@ function onLoad(){
 
 </head>
 <body onload="onLoad()">
-
-<form method="post" action="encrypt.php" target="results" id="password-form"> 
+<br/>
+<form method="post" action="encrypt.php" target="results" id="password-form">
 <input type="text" name="password" id="password" class="form-control" placeholder="Enter Password"/>
-<input type="submit" name="submit" id="submit" class="form-control" value="Encrypt"/> 
+<br/>
+<input type="submit" name="submit" id="submit" class="form-control" value="Encrypt"/>
 </form>
 
 
@@ -29,7 +30,7 @@ function onLoad(){
 <div id="encryptedpassword">
 <!-- <p>MD5: {{encryptPass}} </p> -->
 <!-- <p>SHA1: <?php //echo sha1('{{encryptPass}}'); ?></p> -->
-<iframe src="encrypt.php" name="results" width="100%" height="100%" id="resultframe"/> 
+<iframe src="encrypt.php" name="results" width="100%" height="100%" id="resultframe"/>
 </div>
 
 
@@ -37,4 +38,3 @@ function onLoad(){
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 </body>
 </html>
-
